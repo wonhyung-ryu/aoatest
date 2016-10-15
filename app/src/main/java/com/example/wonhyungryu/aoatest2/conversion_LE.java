d421b051b171d5ad8e2d5424e60af5e707daef48
wonhyung.ryu <wonhyung.ryu@lge.com>
2016-10-15 12:26:14 +0900

    예외처리추가

--- app/src/main/java/com/example/wonhyungryu/aoatest2/conversion_LE.java
+++ app/src/main/java/com/example/wonhyungryu/aoatest2/conversion_LE.java
@@ -1,5 +1,7 @@
 package com.example.wonhyungryu.aoatest2;
 
+import android.util.Log;
+
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 
@@ -9,39 +11,70 @@ import java.nio.ByteOrder;
 
 public class conversion_LE {
     public char byteToChar_LE(byte[] input, int offset ) {
-        return (char)(((input[offset+1]&0xFF)<<8) + (input[offset]&0xFF));
+        try {
+            return (char) (((input[offset + 1] & 0xFF) << 8) + (input[offset] & 0xFF));
+        } catch (Exception e) {
+            Log.e("AOA", "Exception !!!! byteToChar_LE ");
+            return 0;
+        }
     }
 
     public short byteToShort_LE(byte[] input, int offset ) {
-        return (short)(((input[offset+1]&0xFF)<<8) + (input[offset]&0xFF));
+        try {
+            return (short)(((input[offset+1]&0xFF)<<8) + (input[offset]&0xFF));
+        } catch (Exception e) {
+            Log.e("AOA", "Exception !!!! byteToShort_LE ");
+            return 0;
+        }
     }
 
     public int byteToInt_LE(byte[] input, int offset ) {
-        return (int)(((input[offset+3]&0xFF)<<24)+((input[offset+2]&0xFF)<<16)+((input[offset+1]&0xFF)<<8)+(input[offset+0]&0xFF));
+        try {
+            return (int)(((input[offset+3]&0xFF)<<24)+((input[offset+2]&0xFF)<<16)+((input[offset+1]&0xFF)<<8)+(input[offset+0]&0xFF));
+        } catch (Exception e) {
+            Log.e("AOA", "Exception !!!! byteToInt_LE ");
+            return 0;
+        }
     }
 
     public float byteToFloat_LE(byte[] input, int offset) {
         byte[] tmp = new byte[4];
-        System.arraycopy(input, offset, tmp, 0, 4);
+        try {
+            System.arraycopy(input, offset, tmp, 0, 4);
+        } catch (Exception e) {
+            Log.e("AOA", "Exception !!!! byteToFloat_LE ");
+        }
         return ByteBuffer.wrap(tmp).order(ByteOrder.LITTLE_ENDIAN ).getFloat();
     }
 
     public double byteToDouble_LE (byte[] input, int offset ) {
         byte[] tmp = new byte[8];
-        System.arraycopy(input, offset, tmp, 0, 8);
+        try {
+            System.arraycopy(input, offset, tmp, 0, 8);
+        } catch (Exception e) {
+            Log.e("AOA", "Exception !!!! byteToDouble_LE ");
+        }
         return ByteBuffer.wrap(tmp).order(ByteOrder.LITTLE_ENDIAN ).getDouble();
     }
 
     public double byteTolong_LE (byte[] input, int offset ) {
         byte[] tmp = new byte[8];
-        System.arraycopy(input, offset, tmp, 0, 8);
+        try {
+            System.arraycopy(input, offset, tmp, 0, 8);
+        } catch (Exception e) {
+            Log.e("AOA", "Exception !!!! byteTolong_LE ");
+        }
         return ByteBuffer.wrap(tmp).order(ByteOrder.LITTLE_ENDIAN ).getLong();
     }
 
     public char[] byteToCharArray_LE(byte[] input, int offset, int length_byte){
-        char[] tmp = new char[length_byte/2];
-        for (int i=0; i<length_byte/2; i++){
-            tmp[i] = byteToChar_LE(input, offset+2*i);
+            char[] tmp = new char[length_byte / 2];
+        try {
+            for (int i = 0; i < length_byte / 2; i++) {
+                tmp[i] = byteToChar_LE(input, offset + 2 * i);
+            }
+        } catch (Exception e) {
+            Log.e("AOA", "Exception !!!! byteToCharArray_LE ");
         }
         return  tmp;
     }
